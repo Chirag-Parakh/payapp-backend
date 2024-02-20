cd67c68271c19b134b55e877861c847a9fa42b00
Chirag Parakh <cparakh53@gmail.com>
2024-02-20 23:40:10 +0530

    backend sorted

--- routes/accounts.js
+++ routes/accounts.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const { Account } = require('../db');
+router.get('/', (req, res) => {
+    res.json({ message: "get some balance here" });
+})
 
-router.get('/' , (req , res) => {
-    res.json({message : "get some balance here"});
+router.post('/transfer', async (req, res) => {
+    try {
+        const fromUsername = req.body.fromUsername;
+        const toUsername = req.body.toUsername;
+        const amount = req.body.amount;
+        const fromUser = await Account.findOne({ username: fromUsername });
+        if (fromUser.balance < amount) {
+            res.json({ message: "Insufficient balance" })
+        }
+        try {
+            await Account.findOneAndUpdate({ username: fromUsername }, { $inc: { balance: - amount } })
+            await Account.findOneAndUpdate({ username: toUsername }, { $inc: { balance: + amount } })
+            res.json({ message: "transaction successful" });
+        } catch (error) {
+            console.log(error)
+            res.json({ message: "could not complete the transition" })
+        }
+    }
+    catch (error) {
+        res.json({ message: "internal error occured" });
+    }
 })
 
 module.exports = router;
--- routes/signup.js
+++ routes/signup.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const  {User , Account}  = require('../db');
+const { User, Account } = require('../db');
 const UserZodSchema = require('../zod');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
@@ -30,23 +30,24 @@ router.post('/', async (req, res) => {
         });
         const validate = UserZodSchema.safeParse(newUser);
         if (validate.error) {
-            res.status(400).json({ error: validate.error });
+            return res.status(400).json({ error: validate.error });
         }
         newUser.password = await bcrypt.hash(newUser.password, 10);
         await newUser.save();
 
-        const accountInfo= new Account({
-            username:username,
-            balance : 1 + Math.random()*10000
+        const accountInfo = new Account({
+            username: username,
+            balance: 1 + Math.random() * 10000
         });
         await accountInfo.save();
-        
+
         const token = jwt.sign({ username }, JWT_SECRET);
         return res.json({ "message": "User created successfully", "token": `Bearer ${token}` });
     } catch (error) {
         console.error("Error creating user:", error);
-        res.status(500).json({ "error": "Internal server error" });
+        return res.status(500).json({ "error": "Internal server error" });
     }
 });
 
 module.exports = router;
+
